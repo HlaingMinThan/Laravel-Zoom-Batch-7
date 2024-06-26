feee1022ed5b199f7e94433798b120adebe74963
HlaingMinThan <hlaingminthan@92@gmail.com>
2024-06-26 22:37:26 +0700

    day-2

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+
+// data type (6)
+
+$name = "hlaingminthan"; // string
+$number = 13.4; // number -> int,float
+$isMarried = true; // boolean -> true,false
+$cup = null; // null
+$names = ["mgmg", "aungaung"]; //numeric array
+$person = ["name" => "mgmg", "age" => 23]; //associated array
+$car = new stdClass; //object
+$car->name = "Toyota";
+$car->color = "red";
+
+//arithmethic operator (+,-,*,/,%) 
+//comparison operator (>,<,>=,<=,==,===,!=,!==) 
+//gate (and,or,not) (&&,||,!)
+//and -> true - true = true
+//or -> false - false = false
+//not -> true-> false,false -> true
+
+var_dump(10 === '10'); //true
