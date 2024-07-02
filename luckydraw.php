d41171ae9f5b1170331ebdd03923239a6ddc9634
HlaingMinThan <hlaingminthan@92@gmail.com>
2024-07-02 23:51:29 +0700

    day-3

---
+++ luckydraw.php
@@ -0,0 +1,12 @@
+<?php
+
+$hello = "world";
+
+function luckyDraw($name, $age)
+{
+    if ($name === "Mgmg" and $age > 15) {
+        echo "Iphone 15 pro max winner";
+    } else {
+        echo $name . "is not a winner";
+    }
+}
---
+++ people.php
@@ -0,0 +1,20 @@
+<?php
+
+$people = [
+    [
+        "name" => "mgmg",
+        "age" => 20
+    ],
+    [
+        "name" => "mgmg",
+        "age" => 20
+    ],
+    [
+        "name" => "mgmg",
+        "age" => 20
+    ],
+    [
+        "name" => "hlahla",
+        "age" => 20
+    ],
+];
